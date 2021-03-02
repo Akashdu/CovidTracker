ae2434c5114cdc58b1c0f4e20c17ea4111bc7bf1
Akash Dubey <akashdube299@gmail.com>
2021-03-02 15:48:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.covidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
